5d4192dddf5ec2dc6bc913af908b87d5547c5712
Melanie Akinyi <kleinmelanie04@gmail.com>
2025-06-10 12:39:37 +0300

    advanced functions

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+}
+returnFirstTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const returnLastTwoDrivers = function(drivers) {
+    return drivers.slice(2, 4);
+}
+returnLastTwoDrivers(['Antonia', 'Nuru', 'Amari', 'Mo']);
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(multiplier) {
+    return function(fare) {
+        return fare * multiplier;
+    }
+} 
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(arrayOfDrivers, fn) {
+    return fn(arrayOfDrivers);
+}
